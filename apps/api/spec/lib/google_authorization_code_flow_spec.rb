6bdd7aff3d96c96c97f9cf1286c8d04fbfe8d86f
nikola-f <nkl.flny@gmail.com>
2026-03-29 13:16:37 +0900

    add webmock

--- apps/api/spec/lib/google_authorization_code_flow_spec.rb
+++ apps/api/spec/lib/google_authorization_code_flow_spec.rb
@@ -7,11 +7,14 @@ RSpec.describe GoogleAuthorizationCodeFlow do
   let(:mock_client) { instance_double(OAuth2::Client) }
   let(:mock_auth_code) { instance_double(OAuth2::Strategy::AuthCode) }
 
-  before do
+  def stub_credentials
     allow(Rails.application.credentials).to receive(:dig)
       .with(:google, :client_id).and_return("test_client_id")
     allow(Rails.application.credentials).to receive(:dig)
       .with(:google, :client_secret).and_return("test_client_secret")
+  end
+
+  def stub_oauth2_client
     allow(OAuth2::Client).to receive(:new).and_return(mock_client)
     allow(mock_client).to receive(:auth_code).and_return(mock_auth_code)
   end
@@ -21,6 +24,8 @@ RSpec.describe GoogleAuthorizationCodeFlow do
     let(:expected_url) { "https://accounts.google.com/o/oauth2/auth?client_id=test_client_id&..." }
 
     before do
+      stub_credentials
+      stub_oauth2_client
       allow(mock_auth_code).to receive(:authorize_url).and_return(expected_url)
     end
 
@@ -71,11 +76,53 @@ RSpec.describe GoogleAuthorizationCodeFlow do
     end
   end
 
+  describe ".fetch (integration)" do
+    before do
+      stub_credentials
+      stub_request(:post, "https://oauth2.googleapis.com/token")
+        .to_return(
+          status: 200,
+          body: { access_token: "ya29.from_webmock", token_type: "Bearer", expires_in: 3600 }.to_json,
+          headers: { "Content-Type" => "application/json" }
+        )
+    end
+
+    it "returns the access token from Google's response" do
+      result = described_class.fetch(code: "4/test_code", redirect_uri: "https://example.com/callback")
+      expect(result).to eq("ya29.from_webmock")
+    end
+
+    it "sends the code and redirect_uri to Google's token endpoint" do
+      described_class.fetch(code: "4/test_code", redirect_uri: "https://example.com/callback")
+      expect(WebMock).to have_requested(:post, "https://oauth2.googleapis.com/token")
+        .with(body: hash_including("code" => "4/test_code", "redirect_uri" => "https://example.com/callback"))
+    end
+
+    context "when Google returns an error" do
+      before do
+        stub_request(:post, "https://oauth2.googleapis.com/token")
+          .to_return(
+            status: 400,
+            body: { error: "invalid_grant", error_description: "Code was already redeemed." }.to_json,
+            headers: { "Content-Type" => "application/json" }
+          )
+      end
+
+      it "raises FetchError" do
+        expect {
+          described_class.fetch(code: "expired_code", redirect_uri: "https://example.com/callback")
+        }.to raise_error(GoogleAuthorizationCodeFlow::FetchError)
+      end
+    end
+  end
+
   describe ".fetch" do
     let(:code) { "4/test_code" }
     let(:mock_token) { instance_double(OAuth2::AccessToken, token: "ya29.new_token") }
 
     before do
+      stub_credentials
+      stub_oauth2_client
       allow(mock_auth_code).to receive(:get_token).and_return(mock_token)
     end
 
